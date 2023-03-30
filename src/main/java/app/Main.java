493ae2dc75a96caca79163bee79cc1dd037ba2fa
wangchengtao <915129420@qq.com>
2023-03-30 14:59:51 +0800

    数据类型

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import app.zoo.Cat;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class Main {
@@ -21,9 +22,39 @@ public class Main {
         newCat.meow();
         simpleCat.meow();
 
+        int a = 10_0000_000;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toHexString(a));
+        System.out.println(Integer.toOctalString(a));
+
+        System.out.println(0.1 + 0.2);
+        System.out.println(Integer.toBinaryString(Float.floatToIntBits(0.1f)));
+
+
+        BigDecimal c = new BigDecimal("0.1");
+        BigDecimal b = new BigDecimal("0.2");
+        System.out.println(c.add(b));
         int[] arr = {1, 2, 3, 4, 5};
         setArr(arr);
         System.out.println(Arrays.toString(arr));
+
+        short s = 3;
+        int i = 2;
+        double d = 1;
+        double dob = s + i + d;
+        System.out.println((double) i / s);
+
+        int sd = 1;
+        int f = 1;
+        System.out.println(sd == f);
+
+        Integer integer1 = 128;
+        Integer integer2 = 128;
+        System.out.println(integer1 == integer2);
+
+        Cat cat1 = new Cat("曹操");
+        Cat cat2 = new Cat("曹操");
+        System.out.println(cat1.name == cat2.name);
 
     }
 
