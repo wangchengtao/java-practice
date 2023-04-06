e9e67007ce85f60f69b97b38be4bc9c10cbbf3d3
wangchengtao <915129420@qq.com>
2023-04-06 10:53:52 +0800

    静态内部类可以存在静态变量和静态方法 静态内部类只能访问外部类的静态变量和方法 静态内部类访问外部类的普通成员变量和方法, 需实例化外部类
    外部类通过 Inner.xxx 可访问内部类的静态资源
    外部类访问内部类的普通成员变量和方法, 需实例化内部类
    
    综上, 静态内部类在成员内部类的基础上可以定义静态成员和方法

--- src/main/java/app/Outer.java
+++ src/main/java/app/Outer.java
@@ -2,16 +2,25 @@ package app;
 
 public class Outer {
     private int a = 1;
+    private static int b = 2;
 
-    public void test1() {
-        System.out.println("Outer test1 method.");
+    private static void test1() {
+        System.out.println("Outer static test1 method.");
     }
 
-    // 外部类想方位内部类成员变量和方法, 需要先实例化内部类
     public void test() {
+        // 外部类想方位内部类成员变量和方法, 需要先实例化内部类
         Inner inner = new Inner();
         System.out.println(inner.b);
         inner.test();
+
+        // 外部类可直接通过 StaticInner.XXX 类访问内部类的静态资源
+        // 外部类如果想访问内部类的成员变量和普通方法, 需实例化内部类
+        System.out.println(StaticInner.c);
+        StaticInner.test4();
+        // StaticInner staticInner = new StaticInner();
+        // System.out.println(staticInner.d);
+        // staticInner.test();
     }
 
     class Inner {
@@ -28,6 +37,27 @@ public class Outer {
         }
     }
 
+    static class StaticInner {
+        private static String c = "Static Inner";
+        private int d = 10;
+
+        public void test3() {
+            System.out.println("Static Inner method test3");
+        }
+
+        private static void test4() {
+            System.out.println("Static Inner static method test4");
+        }
+
+        public void test() {
+            // 静态内部类可直接访问外部类静态属性和方法
+            // 静态内部类如果想访问外部类普通属性和方法需实例化外部类
+            System.out.println(b);
+            test1();
+            Outer outer = new Outer();
+            outer.test();
+        }
+    }
 
     public static void main(String[] args) {
         Outer outer = new Outer();
